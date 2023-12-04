fc29d4f15863c85e3585bbf07a4812fa232aa31c
Laura Gomez Ruiz <lauragruiz98@gmail.com>
2023-12-04 13:18:13 +0100

    New exercises

---
+++ Swing Exercises/ElectronicDiary/src/electronicdiary/Main.java
@@ -0,0 +1,15 @@
+
+package electronicdiary;
+
+import electronicdiary.View.View;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        View view =  new View();
+        
+        
+    }
+    
+}
